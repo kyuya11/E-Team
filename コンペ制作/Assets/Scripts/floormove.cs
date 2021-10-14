3cb8e9465198e74b4358fd914eac8f4452817255
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2021-10-14 16:45:03 +0900

    今日やったやつ

---
+++ コンペ制作/Assets/Scripts/floormove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floormove : MonoBehaviour
+{
+
+    public float Rotation = 30f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        transform.rotation = Quaternion.Euler(z * Rotation, 0, -x * Rotation);
+
+
+    }
+}
