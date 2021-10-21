ff66bf6f0fd0f6aa6dcad869448496fce134cd54
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2021-10-21 17:06:16 +0900

    shusei

--- コンペ制作/Assets/Scripts/Item.cs
+++ コンペ制作/Assets/Scripts/Item.cs
@@ -7,7 +7,7 @@ public class Item : MonoBehaviour
     //public GameObject itemObject;   //シーンに置いてある拾うアイテムの変数
 
     private void OnCollisionEnter(Collision other)
-    {
+    { 
         if (other.gameObject.tag == "Ball")    //当たった相手のTagがプレイヤーだったら
         {
             //itemObject.gameObject.SetActive(false);     //シーンに置かれたアイテムを消します
