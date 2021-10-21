0115e82a6a81282ac82bb5c4d3b0989d73ef519c
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2021-10-21 11:21:39 +0900

    床の角度調節

--- コンペ制作/Assets/Scripts/floormove.cs
+++ コンペ制作/Assets/Scripts/floormove.cs
@@ -4,23 +4,74 @@ using UnityEngine;
 
 public class floormove : MonoBehaviour
 {
-
+    Transform target;
+    float speed = 1.2f;
+    float step;
     public float Rotation = 30f;
 
+    void Start()
+    {
+        target = GameObject.Find("floor").transform;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        transform.rotation = Quaternion.Euler(z * Rotation, 0, -x * Rotation);
+        //transform.rotation = Quaternion.Euler(z * Rotation, 0, -x * Rotation);
 
         var body = GameObject.Find("Ball").GetComponent<Rigidbody>();
         Rigidbody rigidbody = GameObject.Find("floor").GetComponent<Rigidbody>();
+
+        step = speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Rotation, 0, 0), step);
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-Rotation, 0, 0), step);
+        }
+
+            else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -Rotation), step);
+        }else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, Rotation), step);
+        }
+        
+        else 
+        {
+            Invoke(nameof(neutral),1f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Rotation, 0, -Rotation), step);
+        }else if(Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Rotation, 0,Rotation), step);
+
+        }
+        else if(Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-Rotation, 0, -Rotation), step);
+
+        }
+        else if(Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-Rotation, 0, Rotation), step);
+
+        }
 
         if (Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.LeftArrow))
         {
             body.WakeUp();
         }
     }
-    
+    void neutral()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), step);
+    }
 }
