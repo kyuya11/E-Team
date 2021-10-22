d40090df59d6df18af9ed08733b7153c0dc18334
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2021-10-22 12:29:34 +0900

    Scriptを改善した

--- コンペ制作/Assets/Script/Pause.cs
+++ コンペ制作/Assets/Script/Pause.cs
@@ -2,31 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class Pause : MonoBehaviour
-    {
+public class Pause : MonoBehaviour
+{
 
-        [SerializeField]
-        GameObject pausePanel;
+    [SerializeField]
+    GameObject pausePanel;
 
     private void Start()
     {
         pausePanel.SetActive(false);
     }
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetButton("Start"))
         {
-            if (Input.GetKeyDown(KeyCode.P)|| Input.GetButton("Start"))
+            if (Time.timeScale == 1)
             {
-                if (Time.timeScale == 1)
-                {
-                    Time.timeScale = 0;
-                    pausePanel.SetActive(true);
-                }
-                else
-                {
-                    Time.timeScale = 1;
-                    pausePanel.SetActive(false);
-                }
+                Time.timeScale = 0;
+                pausePanel.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pausePanel.SetActive(false);
             }
         }
-
     }
+
+}
