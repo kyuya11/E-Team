3bfe54be38ab93a58dbecdb7b8130d3a187b6072
itokazu <12006048-k20h@stu.kbc.ac.jp>
2021-11-04 10:49:11 +0900

    PhysicsScript製作開始

---
+++ コンペ制作/Assets/Scripts/Rigidbody/PhysicsScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsScript : MonoBehaviour
+{
+    public float mass;
+    public float v;
+    public float StaticFriction;
+    public float DynamicFriction;
+    public float Rotation;
+    public float acceleration;
+    public float Bounciness;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
