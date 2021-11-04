d4b663b20494071640ca7268866608e1622928cc
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2021-11-04 10:33:26 +0900

    テキスト等が消えていたので修正

--- コンペ制作/Assets/Scripts/floormove.cs
+++ コンペ制作/Assets/Scripts/floormove.cs
@@ -10,6 +10,7 @@ public class floormove : MonoBehaviour
     public float Rotation = 30f;
     float x = 0f;
     float z = 0f;
+    bool getcountdown;
 
     void Start()
     {
@@ -18,75 +19,77 @@ public class floormove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        x = Input.GetAxisRaw("Horizontal");
-        z = Input.GetAxisRaw("Vertical");
-        //transform.rotation = Quaternion.Euler(z * Rotation, 0, -x * Rotation);
-
-        var body = GameObject.Find("Ball").GetComponent<Rigidbody>();
-        Rigidbody rigidbody = GameObject.Find("floor").GetComponent<Rigidbody>();
-
-        step = speed * Time.deltaTime;
-        if (x > 0f)
+        getcountdown = StartCount.CountDownStart();
+        if (getcountdown == false)
         {
-            x = 1f;
+            x = Input.GetAxisRaw("Horizontal");
+            z = Input.GetAxisRaw("Vertical");
+            //transform.rotation = Quaternion.Euler(z * Rotation, 0, -x * Rotation);
+
+            var body = GameObject.Find("Ball").GetComponent<Rigidbody>();
+            Rigidbody rigidbody = GameObject.Find("floor").GetComponent<Rigidbody>();
+
+            step = speed * Time.deltaTime;
+            if (x > 0f)
+            {
+                x = 1f;
+            }
+            else if (x < 0f)
+            {
+                x = -1f;
+
+            }
+            else
+            {
+                x = 0f;
+            }
+            if (z > 0f)
+            {
+                z = 1f;
+
+            }
+            else if (z < 0f)
+            {
+                z = -1f;
+
+            }
+            else
+            {
+                z = 0f;
+            }
+
+            if (x > 0 || x < 0 || z > 0 || z < 0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
+            } else
+            {
+                if (gameObject.transform.localEulerAngles.x > 0)
+                {
+                    for (int i = 120; i < 0; i -= 2)
+                    {
+                        z = i / 120.0f;
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
+
+                    }
+                }
+                else if (gameObject.transform.localEulerAngles.x < 0)
+                {
+                    for (int i = -120; i > 0; i += 2)
+                    {
+                        z = i / 120.0f;
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
+
+                    }
+                }
+                StartCoroutine(DelayCoroutine());
+            }
         }
-        else if (x < 0f)
+        /*private*/ IEnumerator DelayCoroutine()
         {
-            x = -1f;
-
-        }
-        else 
-        {
-            x = 0f;
-        }
-        if (z > 0f)
-        {
-            z = 1f;
-
-        }
-        else if (z < 0f)
-        {
-            z = -1f;
-
-        }
-        else
-        {
+            yield return new WaitForSeconds(1);
             z = 0f;
-        }
-        
-        if (x > 0 || x < 0 || z > 0 || z < 0)
-        {
+            x = 0f;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
-        }else
-        {
-            if (gameObject.transform.localEulerAngles.x > 0)
-            {
-                for (int i = 120; i < 0; i -= 2)
-                {
-                    z = i / 120.0f;
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
-
-                }
-            }
-            else if (gameObject.transform.localEulerAngles.x < 0)
-            {
-                for (int i = -120; i > 0; i += 2)
-                {
-                    z = i / 120.0f;
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
-
-                }
-            }
-            StartCoroutine(DelayCoroutine());
         }
     }
-    private IEnumerator DelayCoroutine()
-    {
-        yield return new WaitForSeconds(1);
-        z = 0f;
-        x = 0f;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(z * Rotation, 0, -x * Rotation), step);
-    }
-    
 }
