a04b2fa88157f885b234f2a34f7d0e2b9b674ff2
itokazu <12006048-k20h@stu.kbc.ac.jp>
2021-11-04 11:06:11 +0900

    スクリプトにコメントを追加

--- コンペ制作/Assets/Scripts/Rigidbody/PhysicsScript.cs
+++ コンペ制作/Assets/Scripts/Rigidbody/PhysicsScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PhysicsScript : MonoBehaviour
 {
-    public float mass;
-    public float v;
-    public float StaticFriction;
-    public float DynamicFriction;
-    public float Rotation;
-    public float acceleration;
-    public float Bounciness;
+    public float mass = 1.0f; //質量
+    public float v = 1.0f; //速度
+    public float StaticFriction = 1.0f; //静止摩擦
+    public float DynamicFriction = 1.0f;  //動摩擦
+    public float Rotation = 1.0f; //回転
+    public float acceleration = 1.0f; //加速度
+    public float Bounciness = 1.0f; //反発率
+    public float Gravity = 9.80665f; //重力加速度
 
     // Start is called before the first frame update
     void Start()
