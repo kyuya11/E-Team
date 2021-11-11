b170c46951033c5fda6829a3a37206d704125d8e
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2021-11-11 14:26:55 +0900

    ボールが転がっている時に煙がでるようにした

--- コンペ制作/Assets/Scripts/Effect.cs
+++ コンペ制作/Assets/Scripts/Effect.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Effect : MonoBehaviour
 {
     [SerializeField]
-    private ParticleSystem footSmoke;
+    private ParticleSystem FootSmoke;
     private Rigidbody rb;
-    
+    private string answerTag = "floor";
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,21 +15,25 @@ public class Effect : MonoBehaviour
 
     void Update()
     {
-        //速度が0.1以上なら
-        if(rb.velocity.magnitude > 0.1f)
+        if (this.gameObject.CompareTag(answerTag))
         {
-            //再生
-            if (!footSmoke.isEmitting)
+
+            //速度が0.1以上なら
+            if (rb.velocity.magnitude > 0.1f)
             {
-                footSmoke.Play();
+                //再生
+                if (!FootSmoke.isEmitting)
+                {
+                    FootSmoke.Play();
+                }
             }
-        }
-        else
-        {
-            //停止
-            if (footSmoke.isEmitting)
+            else
             {
-                footSmoke.Stop();
+                //停止
+                if (FootSmoke.isEmitting)
+                {
+                    FootSmoke.Stop();
+                }
             }
         }
     }
---
+++ コンペ制作/Assets/Scripts/Position.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    private GameObject Ball;  //ボール情報格納用
+
+    private float ballX = 0.0f;
+    private float ballZ = 0.0f;
+    private float ballY = 0.0f;
+
+    private float offsetX = 0.0f;
+    private float offsetY = 0.0f;
+    private float offsetZ = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Ball = GameObject.Find("Ball");
+
+        offsetX = transform.position.x - Ball.transform.position.x;
+        offsetY = transform.position.y - Ball.transform.position.y;
+        offsetZ = transform.position.z - Ball.transform.position.z;
+
+        ballX = Ball.transform.position.x;
+        ballY = Ball.transform.position.y;
+        ballZ = Ball.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ballX = Ball.transform.position.x;
+        ballY = Ball.transform.position.y;
+        ballZ = Ball.transform.position.z;
+
+        transform.position = new Vector3(ballX + offsetX, ballY + offsetY, ballZ + offsetZ);
+    }
+}
