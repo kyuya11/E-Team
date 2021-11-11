f2f747fbc7196709a9ddf946f98469ef422cdbed
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2021-11-11 11:11:54 +0900

    転がっている時のエフェクト未完成

---
+++ コンペ制作/Assets/Scripts/Effect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem footSmoke;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //速度が0.1以上なら
+        if(rb.velocity.magnitude > 0.1f)
+        {
+            //再生
+            if (!footSmoke.isEmitting)
+            {
+                footSmoke.Play();
+            }
+        }
+        else
+        {
+            //停止
+            if (footSmoke.isEmitting)
+            {
+                footSmoke.Stop();
+            }
+        }
+    }
+}
