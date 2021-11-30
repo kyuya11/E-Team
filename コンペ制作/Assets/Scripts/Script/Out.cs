ce91c594e95ec606162440efae3f323bf44efd5f
KBC12006024\kbc12006024 <kaito-hokama-20h@stu.kbc.ac.jp>
2021-11-30 11:36:28 +0900

    落ちたら戻るスクリプト

---
+++ コンペ制作/Assets/Scripts/Script/Out.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Out : MonoBehaviour
+{
+    public  Transform ball;
+    
+    void Start()
+    {
+        ball= GameObject.FindGameObjectWithTag("Ball").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball.position.y < -10)
+        {
+            ball.localPosition = new Vector3(0, 2, 0);
+        }
+    }
+}
