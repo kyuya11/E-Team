ad2e4145be721f3396fb9477aed01ce2500d790b
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2021-12-01 10:52:38 +0900

    スクリプト直した

--- コンペ制作/Assets/Scripts/ResultScript/Timeresult.cs
+++ コンペ制作/Assets/Scripts/ResultScript/Timeresult.cs
@@ -16,12 +16,12 @@ public class Timeresult : MonoBehaviour
         resultseconds = Timer.seconds;
         resultoldseconds = Timer.oldSeconds;
         timerText = GetComponentInChildren<Text>();
-        timerText.text = "経過した時間 　  :  "+ resultminute.ToString("00") + ":" + ((int)resultseconds).ToString("00");
+        timerText.text = "経過した時間 　  :  " + resultminute.ToString("00") + ":" + ((int)resultseconds).ToString("00");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
