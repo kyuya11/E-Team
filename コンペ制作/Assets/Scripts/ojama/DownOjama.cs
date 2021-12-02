517938889c6076c065eb15c743886aaddeafe977
KBC12006017\小浜大和 <towa-kohama-20h@stu.kbc.ac.jp>
2021-12-02 11:41:01 +0900

    ステージ２

---
+++ コンペ制作/Assets/Scripts/ojama/DownOjama.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownOjama : MonoBehaviour
+{
+    Rigidbody rb;
+    Collider m_ObjectCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        m_ObjectCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "floor")
+        {
+            rb.isKinematic = true;
+            m_ObjectCollider.isTrigger = true;
+        }
+    }
+
+
+}
